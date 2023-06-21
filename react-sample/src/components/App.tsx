ae6fb0fca632098132d2a8297f2a7bc9d9ec4def
DDuckyee <dduckyee00@naver.com>
2023-06-21 15:22:33 +0900

    react-redux + atomic pattern test

---
+++ react-sample/src/components/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactWordcloud from "react-wordcloud";
+import { useState } from "react";
+import CounterContainer from "../containers/CounterContainer";
+
+const App: React.FC = () => {
+  return <CounterContainer />;
+};
+
+export default App;
